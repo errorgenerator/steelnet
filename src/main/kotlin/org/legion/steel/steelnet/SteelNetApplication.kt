eeaef0c7272c27598438f9b5acceba117971ed11
errorgenerator <tilman.sattler@outlook.de>
2022-09-14 22:41:26 +0200

    major bugfix

--- src/main/kotlin/org/legion/steel/steelnet/SteelNetApplication.kt
+++ src/main/kotlin/org/legion/steel/steelnet/SteelNetApplication.kt
@@ -30,11 +30,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 @EnableConfigurationProperties(GoogleConfiguration::class)
 @EnableMongoRepositories
+@EnableCaching
+@EnableWebMvc
 class SteelNetApplication
 
 fun main(args: Array<String>) {
